c9f8403b861437b7badfe3ea277698e183d7197c
Dhara <dharadharsandia03@gmail.com>
2025-05-14 16:36:16 -0400

    week2

---
+++ week2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<>
+    <!-- Learning echo  -->
+    <?php
+   echo "<h1>Hello World</h1>";
+   echo "<p> My name is Dhara Dharsandia.</p>";
+   ?>
+
+   <!-- Learning Variables -->
+    <?php
+    $fname = "Dhara";
+    $lname = "Dharsandia";
+
+    echo "<h1> Hello ". $fname ." ". $lname ."</h1>"; 
+    ?>
+</body>
+</html>
